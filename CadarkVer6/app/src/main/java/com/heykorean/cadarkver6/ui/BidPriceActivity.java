e8fe1711d8c376937dd443db7ad33328451841fd
Cadark <hey.hung.vn@gmail.com>
2016-03-04 10:22:37 +0700

    4/03/2016

---
+++ CadarkVer6/app/src/main/java/com/heykorean/cadarkver6/ui/BidPriceActivity.java
@@ -0,0 +1,18 @@
+package com.heykorean.cadarkver6.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.heykorean.cadarkver6.R;
+
+
+/**
+ * Created by Hey.Hung on 3/4/2016.
+ */
+public class BidPriceActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bid_car);
+    }
+}
